a4ca5dcf0a8977e9f788db2c079b7bebc0f389be
Zerro2003 <mugishaelie160@gmail.com>
2025-10-06 19:57:04 +0200

    promises function to return a message

--- javascript_exercises/promises.js
+++ javascript_exercises/promises.js
@@ -1,12 +1,22 @@
-const promise1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject("sorry, something went wrong");
-  }, 2000);
-});
-promise1
-  .then((message) => {
-    console.log(message);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
+// const promise1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject("sorry, something went wrong");
+//   }, 2000);
+// });
+// promise1
+//   .then((message) => {
+//     console.log(message);
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
+const delayer = (msg, delay) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(msg);
+    }, delay);
   });
+};
+delayer("hello", 2000).then((message) => {
+  console.log(message);
+});
