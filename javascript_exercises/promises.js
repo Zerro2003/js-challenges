72f811388c01373c0e6f62301ef5596937351c1f
Zerro2003 <mugishaelie160@gmail.com>
2025-10-08 17:01:38 +0200

    Try promise method (all)

--- javascript_exercises/promises.js
+++ javascript_exercises/promises.js
@@ -20,19 +20,45 @@
 // delayer("hello", 2000).then((message) => {
 //   console.log(message);
 // });
-const delayer = (nbr) => {
-  return new Promise((resolve, reject) => {
-    if (nbr % 2 === 0) {
-      resolve("this number is okay");
-    } else {
-      reject("this number is not okay bcz it is odd");
-    }
-  });
-};
-delayer(2)
-  .then((message) => {
-    console.log(message);
+// const delayer = (nbr) => {
+//   return new Promise((resolve, reject) => {
+//     if (nbr % 2 === 0) {
+//       resolve("this number is okay");
+//     } else {
+//       reject("this number is not okay bcz it is odd");
+//     }
+//   });
+// };
+// delayer(2)
+//   .then((message) => {
+//     console.log(message);
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
+
+const prom1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("promise 1 resolved");
+  }, 1000);
+});
+
+const prom2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("promise 2 resolved");
+  }, 2000);
+});
+
+const prom3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("promise 3 resolved");
+  }, 1500);
+});
+
+Promise.all([prom1, prom2, prom3])
+  .then((messages) => {
+    console.log("All promises resolved:", messages);
   })
   .catch((error) => {
-    console.error("Error:", error);
+    console.error("Error in one of the promises:", error);
   });
