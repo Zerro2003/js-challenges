7e7f47c9f525913018e83c01926e875d3836a10e
Zerro2003 <mugishaelie160@gmail.com>
2026-01-12 17:12:41 +0200

    transforming regural promise function to asyn ex1

--- javascript_exercises/Asynchronous.js
+++ javascript_exercises/Asynchronous.js
@@ -1,12 +1,36 @@
+// const g = async () =>{
+//     try{
+//         let info = await fetch("https://jsonplaceholder.typicode.com/posts")
+//         let data = await info.json()
+//         console.log(data)
+//     }
+//     catch(err){
+//         console.log(err)
+//     }
+// }
+// g()
 
-const g = async () =>{
-    try{
-        let info = await fetch("https://jsonplaceholder.typicode.com/posts")
-        let data = await info.json()
-        console.log(data)
-    }
-    catch(err){
-        console.log(err)
-    }
+// function loadJson(url) {
+//   return fetch(url)
+//     .then(response => {
+//       if (response.status == 200) {
+//         return response.json();
+//       } else {
+//         throw new Error(response.status);
+//       }
+//     });
+// }
+
+// loadJson('https://javascript.info/no-such-user.json')
+//   .catch(alert);
+
+async function loadJson(url) {
+  try {
+    let Answer = await fetch(url);
+    let data = await Answer.json();
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
 }
-g()
+loadJson("https://javascript.info/no-such-user.json");
