a24e30423d235e7fca9d9748e0190f0bec4106e9
Zerro2003 <mugishaelie160@gmail.com>
2026-01-20 20:54:40 +0200

    adds program working on abortController

--- javascript_exercises/Asynchronous.js
+++ javascript_exercises/Asynchronous.js
@@ -35,41 +35,68 @@
 // }
 // loadJson("https://javascript.info/no-such-user.json");
 
-class HttpError extends Error {
-  constructor(response) {
-    super(`${response.status} for ${response.url}`);
-    this.name = "HttpError";
-    this.response = response;
-  }
-}
+// class HttpError extends Error {
+//   constructor(response) {
+//     super(`${response.status} for ${response.url}`);
+//     this.name = "HttpError";
+//     this.response = response;
+//   }
+// }
 
-function loadJson(url) {
-  return fetch(url).then((response) => {
-    if (response.status == 200) {
-      return response.json();
-    } else {
-      throw new HttpError(response);
-    }
-  });
-}
+// function loadJson(url) {
+//   return fetch(url).then((response) => {
+//     if (response.status == 200) {
+//       return response.json();
+//     } else {
+//       throw new HttpError(response);
+//     }
+//   });
+// }
 
-// Ask for a user name until github returns a valid user
-function demoGithubUser() {
-  let name = prompt("Enter a name?", "iliakan");
+// // Ask for a user name until github returns a valid user
+// function demoGithubUser() {
+//   let name = prompt("Enter a name?", "iliakan");
 
-  return loadJson(`https://api.github.com/users/${name}`)
-    .then((user) => {
-      alert(`Full name: ${user.name}.`);
-      return user;
-    })
-    .catch((err) => {
-      if (err instanceof HttpError && err.response.status == 404) {
-        alert("No such user, please reenter.");
-        return demoGithubUser();
-      } else {
-        throw err;
-      }
+//   return loadJson(`https://api.github.com/users/${name}`)
+//     .then((user) => {
+//       alert(`Full name: ${user.name}.`);
+//       return user;
+//     })
+//     .catch((err) => {
+//       if (err instanceof HttpError && err.response.status == 404) {
+//         alert("No such user, please reenter.");
+//         return demoGithubUser();
+//       } else {
+//         throw err;
+//       }
+//     });
+// }
+
+// demoGithubUser();
+
+let theController = new AbortController();
+let signal = theController.signal;
+let myF = async () => {
+  try {
+    let res = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+      signal,
     });
-}
+    if (!res.ok) {
+      throw new Error("this will not be available");
+    } else {
+      let data = await res.json();
+      console.log(data);
+    }
+  } catch (err) {
+    if (err.name === "AbortError") {
+      console.log("this is taking too long to load");
+    } else {
+      console.error(err);
+    }
+  }
+};
+setTimeout(myF, 3000);
 
-demoGithubUser();
+setTimeout(() => {
+  theController.abort();
+}, 1000);
