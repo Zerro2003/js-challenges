a2ac239fb86810d55cbc0b006a337908e98bcc12
Zerro2003 <mugishaelie160@gmail.com>
2026-02-04 16:28:20 +0200

    usign abortTimeout

--- javascript_exercises/Asynchronous.js
+++ javascript_exercises/Asynchronous.js
@@ -73,26 +73,36 @@
 // }
 
 // demoGithubUser();
-let myF = async (time) => {
-  try {
-    let signal = AbortSignal.timeout(time);
-    let post = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
-      signal,
-    });
-    if (!post.ok) {
-      throw new Error("this error caused by time delay");
-    }
-    let data = await post.json();
-    console.log(data);
-  } catch (err) {
-    if (err.name === "TimeoutError") {
-      console.log(`this is taking longer than ${time} seconds`);
-    } else {
-      console.log("please there is error", err.message);
-    }
-  }
-};
-myF(500);
+// let myF = async (time) => {
+//   try {
+//     let signal = AbortSignal.timeout(time);
+//     let post = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+//       signal,
+//     });
+//     if (!post.ok) {
+//       throw new Error("this error caused by time delay");
+//     }
+//     let data = await post.json();
+//     console.log(data);
+//   } catch (err) {
+//     if (err.name === "TimeoutError") {
+//       console.log(`this is taking longer than ${time} seconds`);
+//     } else {
+//       console.log("please there is error", err.message);
+//     }
+//   }
+// };
+// myF(500);
+
+// fetch("https://jsonplaceholder.typicode.com/posts/1")
+//   .then((ans) => {
+//     if (!ans.ok) {
+//       throw new Error("There is issue in fetching data");
+//     }
+//     return ans.json();
+//   })
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
 
 // Fetch 3 APIs sequentially and log results in order, then refactor to
 // fetch them in parallel while preserving order.
@@ -156,3 +166,49 @@ myF(500);
 //   console.log("bra bra bra");
 // };
 // xml.send();
+
+// let xhr = new XMLHttpRequest();
+// xhr.open("GET", "https://jsonplaceholder.typicode.com/posts/1");
+// xhr.onload = () => {
+//   if (xhr.status >= 200 && xhr.status < 300) {
+//     const data = JSON.parse(xhr.responseText);
+//     console.log(data);
+//   } else {
+//     console.error("server error", xhr.status);
+//   }
+// };
+// xhr.onerror = () => {
+//   console.log("Something went wrong");
+// };
+
+// xhr.send();
+
+// const thisOne = () => {
+//   console.log(4 + 5);
+// };
+// let interv = setInterval(thisOne, 2000);
+// setTimeout(() => {
+//   clearInterval(interv);
+// }, 6000);
+
+const myFunc = async (myTime) => {
+  try {
+    const signal = AbortSignal.timeout(myTime);
+    const ourData = await fetch(
+      "https://jsonplaceholder.typicode.com/posts/1",
+      { signal },
+    );
+    if (!ourData.ok) {
+      throw new Error("sha ibintu byagoranye");
+    }
+    const myData = await ourData.json();
+    console.log(myData);
+  } catch (err) {
+    if (err.name === "TimeoutError") {
+      console.log("well, this is taking soo long to load");
+    } else {
+      console.error(err);
+    }
+  }
+};
+myFunc(10000);
